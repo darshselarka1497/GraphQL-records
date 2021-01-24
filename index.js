ea9130365c62f8ed941b4d2d9ba3d9db809953d9
dselarka <dselarka@iu.edu>
2021-01-24 17:37:40 +0530

    set up express server

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("working graphql")
+})
+
+const port = 8000;
+
+app.listen(port, console.log("Running at "+port));
