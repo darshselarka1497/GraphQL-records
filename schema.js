ad5c2d5ef05deb1751c2354195bd2eb45228e3f2
dselarka <dselarka@iu.edu>
2021-01-24 18:13:03 +0530

    set up schema

--- schema.js
+++ schema.js
@@ -1,7 +1,31 @@
 import {buildSchema} from 'graphql';
 
 const schema = buildSchema(`
-    
+    type Job{
+        id: ID
+        jobName: String
+        category: String
+        salary: Int
+        level: String
+        email: String
+        location: Location
+        supervisors: [supervisor]
+    } 
+    type supervisor{
+        firstName: String
+        lastName: String
+        experience: Int
+    }
+
+    enum Location{
+        inOffice
+        remote
+        hybrid
+    }
+
+    type Query{
+        getJob(id: ID): Job
+    }
 `);
 
 export default schema;
