0f774c7061ed2a6ff98163921188a7fd19f4af00
dselarka <dselarka@iu.edu>
2021-01-24 18:45:24 +0530

    implemented resolvers

--- resolvers.js
+++ resolvers.js
@@ -4,13 +4,26 @@ class Job{
     constructor(id,{
         jobName, category, salary, level, email, location, supervisors
     }){
-        this.id = id;
-        this.jobName = jobName;
-        this.category = category;
-        this.salary = salary;
-        this.level = level;
-        this.email = email;
-        this.location = location;
-        this.supervisors = supervisors;
+        this.id = id
+        this.jobName = jobName
+        this.category = category
+        this.salary = salary
+        this.level = level
+        this.email = email
+        this.location = location
+        this.supervisors = supervisors
+    }
+}
+
+const jobHolder = {};
+
+const resolvers = {
+    getJob: ({id})  => {
+        return new Job(id, jobHolder[id]);
+    },
+    createJob: ({input}) => {
+        let id = nanoid();
+        jobHolder[id] = input;
+        return new Job(id, input);
     }
 }
